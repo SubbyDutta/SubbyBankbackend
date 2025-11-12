0cf06c77f80cfca9e03a35a64b30d7cd81cd3f8a
SubbyDutta <subhamdutta4289@gmail.com>
2025-11-12 17:27:13 +0530

    Update LoanService.java

--- src/main/java/backend/backend/service/LoanService.java
+++ src/main/java/backend/backend/service/LoanService.java
@@ -79,18 +79,21 @@ public class LoanService {
         int amount_to_pay=0;
         int rate=0;
 
-        if(req.getCreditScore()>=750 )
+        if(req.getCreditScore()>=800 )
         {
-           rate= 10;
+           rate= 8;
+        }else if(req.getCreditScore()>=750)
+        {
+            rate=10;
         }else if(req.getCreditScore()>=700)
         {
             rate=15;
-        }else if(req.getCreditScore()>=650)
+        }else if(req.getCreditScore()>=600)
         {
             rate=20;
-        }else if(req.getCreditScore()>=500)
+        }else if(req.getCreditScore()>=300)
         {
-            rate=25;
+            rate=30;
         }
 
         amount_to_pay= (int) ((int) (requestedAmount*rate/100)+requestedAmount);
