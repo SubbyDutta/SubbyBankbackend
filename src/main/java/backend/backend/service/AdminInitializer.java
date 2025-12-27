696137c0cf31e59067cb1e8e045118d8129dc7f4
SubbyDutta <subhamdutta4289@gmail.com>
2025-12-28 00:15:43 +0530

    Updated code

--- src/main/java/backend/backend/service/AdminInitializer.java
+++ src/main/java/backend/backend/service/AdminInitializer.java
@@ -1,33 +1,44 @@
 package backend.backend.service;
 
-// AdminInitializer.java
 
 
+
+import backend.backend.configuration.AdminProperties;
 import backend.backend.model.User;
 import backend.backend.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AdminInitializer {
-
+   private final AdminProperties adminProperties;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
-    public AdminInitializer(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+    private String username;
+    private String password;
+    @PostConstruct
+    public void init()
+    {
+        username=adminProperties.getUsername();
+        password= adminProperties.getPassword();
     }
+
 
     @Bean
     CommandLineRunner initAdmin() {
         return args -> {
-            if (!userRepository.existsByUsername("ADMIN123")) {
+            if (!userRepository.existsByUsername(username)) {
                 User admin = new User();
                 admin.setUsername("ADMIN123");
-                admin.setPassword(passwordEncoder.encode("123456"));
+                admin.setFirstname("ADMIN");
+                admin.setLastname("ADMIN");
+
+                admin.setPassword(passwordEncoder.encode(password));
                 admin.setRole("ADMIN");
                 admin.setEmail("admin@gmail.com");
                 admin.setMobile("8017798434");
