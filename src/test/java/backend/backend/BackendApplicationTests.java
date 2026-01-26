58ba564ff392f45de309832cd61e6a9b6a30d459
SubbyDutta <subhamdutta4289@gmail.com>
2026-01-26 17:42:32 +0530

    fix CI: use H2 via test profile

--- src/test/java/backend/backend/BackendApplicationTests.java
+++ src/test/java/backend/backend/BackendApplicationTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(properties = "spring.profiles.active=test")
 class BackendApplicationTests {
 
 	@Test
